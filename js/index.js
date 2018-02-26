2bbb84b6d73fb944861eaf9f7365bdcdc34d345e
Valeriy Yurchenko <leon61@ukr.net>
2018-02-26 17:10:21 +0000

    v.1

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// // alert('Hello World!');
+// //document.write(x);
+
+// let x = Math.floor (Math.random()*100);
+// document.write(x);
+// document.write('<br>');
+
+// let productTitle = 'Mazda RX-8';
+// let productPrice = 40000;
+// let isAvailable = true;
+
+// document.write(productTitle);
+// document.write('<br>');
+// document.write(productPrice);
+// document.write('<br>');
+// document.write(isAvailable);
+// document.write('<br>');
+// document.write('New price:');
+// document.write('<br>');
+// productPrice = productPrice * 0.1;
+// document.write(productPrice);
+// document.write('<br>');
+// let productColor;
+// document.write('Product Color');
+// document.write('<br>');
+// document.write(productColor);
+
+
+// let obj = {};
+// document.write('obj');
+// document.write('<br>');
+
+// let sym = Symbol();
+// document.write('sym');
+
+
+// console.log(Math.sqrt(4));
+// console.log(Math.sqrt(-4));
+// console.log(-4/0);
+
+const userName = prompt('Enter your name');
+console.log('Hello, ' + userName);
+
+const userAge = prompt('Enter your age');
+console.log('Your age is ' + userAge);
+const newAge = userAge + 1;
+console.log('Next year you will be ' + newAge);
